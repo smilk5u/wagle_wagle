3138b2fcfc85097e7c3e027d8d5a7c7b522d407f
amilk5u <amilk5u@naver.com>
2023-09-13 22:12:51 +0900

    style : onboarding 페이지 스타일 변경

--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -5,35 +5,58 @@ const Onboarding = () => {
     window.location.pathname = "/login";
   };
   return (
-    <Section>
-      <div>Onboarding</div>
-      <button onClick={moveToLogin}>시작하기</button>
-    </Section>
+    <OnboardingMain>
+      <div>
+        <strong>와글와글</strong>
+        <p>
+          기와의‘와’ 랑 한글의‘글’을 합쳐 와글와글이란 이름이 탄생했습니다! <br />
+          사전적 의미는 ‘사람이 한곳에 많이 모여 잇따라 떠들거나<br />
+          움직이는 소리 또는 그 모양’으로<br />
+          이곳에서 많은 사람들이 한글날을 기억하며, ‘와글와글’떠들기 바랍니다.
+        </p>
+        <button type="button" onClick={moveToLogin}>와글와글 시작하기</button>
+      </div>
+    </OnboardingMain>
   );
 };
 
 export default Onboarding;
-
-const Section = styled.section`
+ 
+const OnboardingMain = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
   align-items: center;
-  gap: 30px;
   > div {
-    font-size: 50px;
+    width: 1280px;
+    margin: 0 auto;
+    padding: 0 20px;
   }
-  > button {
+  strong {
+    font-family: var(--font-hunmin);
+    font-size: 110px;
+    font-weight: 400;
+  }
+  p {
+    margin: 50px 0 0;
+    font-family: var(--font-hunmin-saeron);
+    font-size: 24px;
+    line-height: 40px;
+    letter-spacing: -1px;
+  }
+  
+  button {
+    font-family: var(--font-hunmin);
+    font-weight: 400;
     height: 64px;
-    padding: 10px;
+    padding: 21px 107px;
+    margin: 100px 0 0;
     box-sizing: border-box;
     color: white;
-    font-size: large;
-    background-color: #e75852;
-    border: none;
+    font-size: 20px;    
+    background-color: var(--btn-main-color);
     border-radius: 6px;
-    cursor: pointer;
+    transition: all, .4s;
+    &:hover { background-color: #e24840; }
   }
 `;
